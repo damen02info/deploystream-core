a53701890723882428edf0f4c68749e248de9b95
Dani <damen02.info@gmail.com>
2026-05-25 01:26:18 +0200

    Initial commit

---
+++ src/main/java/com/mdz/deploystream/DeploystreamCoreApplication.java
@@ -0,0 +1,13 @@
+package com.mdz.deploystream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploystreamCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploystreamCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdz/deploystream/DeploystreamCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdz.deploystream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploystreamCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
